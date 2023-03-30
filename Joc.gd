c36bc50f528a444c443d4b11ad6084e7c73f1e21
mitghnoms <128025998+mitghnoms@users.noreply.github.com>
2023-03-30 19:56:56 +0200

    moviment

--- Joc.gd
+++ Joc.gd
@@ -1,31 +1,31 @@
 extends Node2D
-
-const fitxer_dades = "user://dades.txt"
-var punts = 25
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	punts = 2
-	
-	var punts_max = llegeix_punts()
-	if punts > punts_max:
-		punts_max = punts
-		desa_punts(punts_max)
-	
-
-
-
-func desa_punts(p):
-	var fitxer = File.new()
-	fitxer.open(fitxer_dades, File.WRITE)
-	fitxer.store_string(var2str(p))
-	fitxer.close()
-
-
-func llegeix_punts():
-	var fitxer = File.new()
-	fitxer.open(fitxer_dades, File.READ)
-	var max_punts = str2var(fitxer.get_as_text())
-	fitxer.close()
-	return max_punts
+#
+#const fitxer_dades = "user://dades.txt"
+#var punts = 25
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	punts = 2
+#
+#	var punts_max = llegeix_punts()
+#	if punts > punts_max:
+#		punts_max = punts
+#		desa_punts(punts_max)
+#
+#
+#
+#
+#func desa_punts(p):
+#	var fitxer = File.new()
+#	fitxer.open(fitxer_dades, File.WRITE)
+#	fitxer.store_string(var2str(p))
+#	fitxer.close()
+#
+#
+#func llegeix_punts():
+#	var fitxer = File.new()
+#	fitxer.open(fitxer_dades, File.READ)
+#	var max_punts = str2var(fitxer.get_as_text())
+#	fitxer.close()
+#	return max_punts
 
